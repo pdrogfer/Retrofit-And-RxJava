f8dbb8ff5dcfa7798aa2813db392644cd7cb31ae
Pedro Gonzalez <pdro74@hotmail.com>
2019-11-19 09:39:01 +0100

    another commit

--- app/src/main/java/com/example/retrofitandrxjava/ApiRetrofit.java
+++ app/src/main/java/com/example/retrofitandrxjava/ApiRetrofit.java
@@ -6,6 +6,7 @@ import io.reactivex.Observable;
 import io.reactivex.Single;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Path;
 
 public interface ApiRetrofit {
